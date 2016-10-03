a9140a298792b3c90e8544b1ea773368ad74ff60
Sergei Lamkov <lamkov@gmail.com>
2016-10-03 22:38:46 +0300

    changes

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,56 +1,87 @@
+var newInterval;
+
+function openModal() {	
+	add();
+	startTimer();
+}
+
 function add(){
-    document.getElementById("container-overlay").style.display = "block";
-    document.querySelector(".view_2_main").style.opacity = "1";
+	document.getElementById("container-overlay").style.display = "block";
+	document.querySelector(".view_2_main").className += " open";
 }
 
-function close() {
-    document.getElementById("container-overlay").style.display = "none";
-    document.querySelector(".view_2_main").style.opacity = "0";
+function closeModal() {
+	document.getElementById("container-overlay").style.display = "none";	
+	document.querySelector(".view_2_main").classList.remove("open");
+	clearInterval(newInterval);
 }
-var cross = document.getElementsByClassName(".close");
-cross.onclick = close;
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+function startTimer() {
+	var duration = 60 * 5, 
+		timer = duration, minutes, seconds;		
+	
+	document.querySelector('#time').textContent = '';	//clean timer
+	
+	newInterval = setInterval(function () {
+		minutes = parseInt(timer / 60, 10)
+		seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+		document.querySelector('#time').textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
+		if (--timer < 0) {
+			timer = duration;
+            reset();
+            checkboxes();
+            closeModal();
+		}
+	}, 1000);
+}
+
+function checkboxes(){
+    var inputElems = document.getElementsByClassName("checkbox_count");
+        count = 0;
+
+    for (var i=0; i<inputElems.length; i++) {
+        if (inputElems[i].type === "checkbox" && inputElems[i].checked === true){
+            count++;
         }
-    }, 1000);
+        document.getElementById("quantity").innerHTML = count;
+    }}
+
+function reset() {
+    document.getElementById("basic_form").reset();
+
 }
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+
+function creator() {
+	// TODO: do somethink
+    var i;
+    var selection = document.getElementById("checkbox" + i);
+    var selected = document.getElementById("form" + i);
+
+    var cln = selected.cloneNode(true);
+    document.getElementById("selection_area").appendChild(cln);
+}
 
 
-/*window.onload = function count() {
-    var counting = document.querySelectorAll('input[type="checkbox"]:checked').length;
-    document.getElementById("quantity").innerHTML = counting;
-}*/
 
-
-    var copy = document.querySelector("myList2").lastChild;
-    var cln = copy.cloneNode(true);
-    document.getElementById("myList1").appendChild(cln);
+/*
+var copy = document.querySelector("myList2").lastChild;
+var cln = copy.cloneNode(true);
+document.getElementById("myList1").appendChild(cln);
 
 function copy() {
-    var inputs = document.getElementsByClassName("checker");
+	var inputs = document.getElementsByClassName("checker");
 
-    for (var i = usersArray.length - 1; i >= 0; i--) {
-        if(inputs[i].checked) {
-            usersArray.splice(i, 1);
-        }
-    }
+	for (var i = usersArray.length - 1; i >= 0; i--) {
+		if(inputs[i].checked) {
+			usersArray.splice(i, 1);
+		}
+	}
 
-    drawTable();
+	drawTable();
 }
+*/
